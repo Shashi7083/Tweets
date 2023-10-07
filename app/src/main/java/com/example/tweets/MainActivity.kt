7a8dd3369ef0611914f966c913cdd06aa16a78b0
archive-nptel <violentsr005@gmail.com>
2023-10-07 16:57:50 +0530

    v1.0.1

--- app/src/main/java/com/example/tweets/MainActivity.kt
+++ app/src/main/java/com/example/tweets/MainActivity.kt
@@ -3,17 +3,28 @@ package com.example.tweets
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -38,12 +49,32 @@ class MainActivity : ComponentActivity() {
                     topBar = {
                        TopAppBar(
                            colors = TopAppBarDefaults.smallTopAppBarColors(
-                               containerColor = Color.Black,
+                               containerColor = Color.White,
                                titleContentColor = Color.White
                            ),
                            title = {
-                                 Text(text = "Tweets")
-                       })
+                                 Text(text = "Tweets",
+                                     modifier = Modifier
+                                         .fillMaxWidth(1f)
+                                         .padding(20.dp,0.dp,20.dp,0.dp),
+                                     color = Color.Black,
+                                     style = TextStyle(
+                                         textAlign = TextAlign.Center,
+                                         fontSize = 20.sp,
+                                         fontWeight = FontWeight.Bold
+                                     )
+
+                                 )
+                       },
+                           navigationIcon = {
+                               Box(modifier = Modifier
+                                   .padding(15.dp)
+                               )
+                              Image(
+                                  imageVector = Icons.Default.Home,
+                                  contentDescription =  "Home"
+                              )
+                           })
                     }
                 ) {
                     Box(modifier = Modifier.padding(it)){
